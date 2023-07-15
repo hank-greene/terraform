607f62da2ae4467eef1f3980ed799ccdceb1d676
hank greene <hank.greene@gmail.com>
2023-07-15 02:22:07 +0000

    start building a module based deployment

--- 01-environments/02-staging/variables.tf
+++ 01-environments/02-staging/variables.tf
@@ -3,23 +3,23 @@
 variable "vpc_name" {
     description = "vpc_name"
     type = string
-    default = "dev-drupal"
+    default = "staging-drupal"
 }
 
 variable "vpc_cidr_block" {
-    default = "10.1.0.0/16"
+    default = "10.2.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
     type = list(string)
     description = "public subnet cidr values"
-    default = ["10.1.1.0/24","10.1.2.0/24","10.1.3.0/24"]
+    default = ["10.2.1.0/24","10.2.2.0/24","10.2.3.0/24"]
 }
 
 variable "private_subnet_cidrs" {
     type = list(string)
     description = "private subnet cidr values"
-    default = ["10.1.4.0/24","10.1.5.0/24","10.1.6.0/24"]
+    default = ["10.2.4.0/24","10.2.5.0/24","10.2.6.0/24"]
 }
 
 variable "azs" {
